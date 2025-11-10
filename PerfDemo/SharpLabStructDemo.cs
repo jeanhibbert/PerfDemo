18217ce519706641bc0cddca7efa4cdfeb29e925
jean.hibbert <jean.hibbert@gmail.com>
2025-11-10 20:42:34 +0000

    Sharplabfile

---
+++ PerfDemo/SharpLabStructDemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDemo;
+public struct MyStruct
+{
+    public int X;
+    public int GetValue() => X;
+    public void Mutate() => X++;
+}
+
+public ref struct MyRefStruct
+{
+    public int X;
+    public int GetValue() => X;
+    public void Mutate() => X++;
+}
+
+public readonly ref struct MyReadOnlyStruct
+{
+    public readonly int X;
+    public readonly int GetValue() => X;
+    //public void Mutate() => X++;
+}
+
+internal class SharpLabStructDemo
+{
+    public void ProcessStructWithIn()
+    {
+        const int iterations = 1_000_000;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            var myStruct = new MyReadOnlyStruct();
+            ProcessStructWithIn(myStruct);
+        }
+    }
+
+    private void ProcessStructWithIn(in MyReadOnlyStruct myStruct)
+    {
+        myStruct.GetValue();
+    }
+}
